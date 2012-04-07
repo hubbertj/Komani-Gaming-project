35dd93bc947e6feaad65c8938c2e4514950a0760
Jerum Hubbert <hubbertj@gmail.com>
2012-04-07 09:24:48 -0700

    Added XML class

---
+++ Konami Games Project ServerSide/src/test/Konami/XmlReceived.java
@@ -0,0 +1,22 @@
+package test.Konami;
+
+public class XmlReceived {
+	String xml;
+	
+	public XmlReceived (String Xml){
+		this.xml = Xml;
+	}
+	public String updateNameGrid(){
+		return xml;
+		
+	}
+	public String updateAddressGrid(){
+		return xml;
+		
+	}
+	public String updateCommandGrid(){
+		return xml;
+		
+	}
+
+}
